0b25ac1b655ecd117669a2fd804e5d9bb663983f
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-03-29 23:19:34 +0800

    Implement VisitDeclaration

--- Group1_InterpreterConsole/Group1_InterpreterConsole/CodeVisitor/Visitor.cs
+++ Group1_InterpreterConsole/Group1_InterpreterConsole/CodeVisitor/Visitor.cs
@@ -2,6 +2,7 @@
 using Group1_InterpreterConsole.Contents;
 using Group1_InterpreterConsole.Methods;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Group1_InterpreterConsole.CodeVisitor
 {
@@ -155,6 +156,10 @@ namespace Group1_InterpreterConsole.CodeVisitor
             {
                 return VisitDisplay(context.display());
             }
+            else if (context.declaration() != null)
+            {
+                return VisitDeclaration(context.declaration());
+            }
 
             //else if (context.conditional() != null)
             //{
@@ -177,73 +182,77 @@ namespace Group1_InterpreterConsole.CodeVisitor
                 throw new Exception("Unknown statement type");
             }
         }
-        
-        public override object? VisitDeclaration([NotNull] CodeParser.DeclarationContext context)
+
+        public override List<object?>? VisitDeclaration([NotNull] CodeParser.DeclarationContext context)
         {
             var type = context.type().GetText();
-            var varName = context.IDENTIFIER().Select(x => x.GetText()).ToArray();
+            var identifiers = context.IDENTIFIER().Select(x => x.GetText()).ToList();
 
-            var varValue = context.expression();
+            var values = context.expression();
 
-            for(int i = 0; i < varName.Length; i++)
+            for (int j = 0; j < values.Count(); j++)
             {
-                if (Variables.ContainsKey(varName[i]))
+                var value = values[j].GetText();
+
+                if (j >= identifiers.Count())
                 {
-                    Console.WriteLine($"Variable '{varName}' is already defined!");
+                    Console.WriteLine($"Too many values specified for variable '{string.Join(",", identifiers)}'");
+                    break;
+                }
+
+                var identifier = identifiers[j];
+
+                if (Variables.ContainsKey(identifier))
+                {
+                    Console.WriteLine($"Variable '{identifier}' is already defined!");
                 }
                 else
                 {
                     if (type.Equals("INT"))
                     {
-                        if (int.TryParse(varValue.ToString(), out int intValue))
+                        if (int.TryParse(value, out int intValue))
                         {
-                            Variables[varName[i]] = intValue;
+                            Variables[identifier] = intValue;
                         }
                         else
                         {
-                            int value;
-                            bool success = int.TryParse(varValue.ToString(), out value);
-                            if (!success)
-                            {
-                                Console.WriteLine($"Invalid value for integer variable '{varName}'");
-                            }
+                            Console.WriteLine($"Invalid value for integer variable '{identifier}'");
                         }
                     }
                     else if (type.Equals("FLOAT"))
                     {
-                        if (float.TryParse(varValue.ToString(), out float floatValue))
-                            return Variables[varName[i]] = floatValue;
+                        if (float.TryParse(value, out float floatValue))
+                            Variables[identifier] = floatValue;
                         else
-                            Console.WriteLine($"Invalid value for float variable '{varName}'");
+                            Console.WriteLine($"Invalid value for float variable '{identifier}'");
                     }
                     else if (type.Equals("BOOL"))
                     {
-                        if (bool.TryParse(varValue.ToString(), out bool boolValue))
-                            return Variables[varName[i]] = boolValue;
+                        if (bool.TryParse(value, out bool boolValue))
+                            Variables[identifier] = boolValue;
                         else
-                            Console.WriteLine($"Invalid value for boolean variable '{varName}'");
+                            Console.WriteLine($"Invalid value for boolean variable '{identifier}'");
                     }
                     else if (type.Equals("CHAR"))
                     {
-                        var charValue = varValue.ToString();
-                        if (charValue?.Length == 3 && charValue[i] == '\'' && charValue[2] == '\'')
-                            return Variables[varName[i]] = charValue[1];
+                        var charValue = value;
+                        if (charValue?.Length == 3 && charValue[0] == '\'' && charValue[2] == '\'')
+                            Variables[identifier] = charValue[1];
                         else
-                            Console.WriteLine($"Invalid value for character variable '{varName}'");
+                            Console.WriteLine($"Invalid value for character variable '{identifier}'");
                     }
                     else if (type.Equals("STRING"))
                     {
-                        return Variables[varName[i]] = varValue.ToString();
+                        Variables[identifier] = value;
                     }
                     else
                     {
                         Console.WriteLine($"Invalid variable type '{type}'");
                     }
                 }
-
             }
 
-            return null;
+            return new List<object?>(); // add this to the end of the for loop
         }
 
         public override object? VisitVariable_dec([NotNull] CodeParser.Variable_decContext context)
@@ -308,7 +317,17 @@ namespace Group1_InterpreterConsole.CodeVisitor
 
         public override object? VisitIdentifierExpression([NotNull] CodeParser.IdentifierExpressionContext context)
         {
-            return null;
+            var identifier = context.IDENTIFIER().GetText();
+
+            if (Variables.ContainsKey(identifier))
+            {
+                return Variables[identifier];
+            }
+            else
+            {
+                Console.WriteLine($"Variable '{identifier}' is not defined!");
+                return null;
+            }
         }
     }
 }

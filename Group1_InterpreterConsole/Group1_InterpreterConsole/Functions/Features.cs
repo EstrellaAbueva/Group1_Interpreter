c5011f0f947cccad0169eade2dfff104650ab3cb
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-04-09 23:39:15 +0800

    Update Increment and Decrement ErrorHandlers

--- Group1_InterpreterConsole/Group1_InterpreterConsole/Functions/Features.cs
+++ Group1_InterpreterConsole/Group1_InterpreterConsole/Functions/Features.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Group1_InterpreterConsole.ErrorHandling;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,49 +11,49 @@ namespace Group1_InterpreterConsole.Functions
 {
     public class Features
     {
-        public static object? Increment(Dictionary<string, object?> dictionary, Dictionary<string, object?> dictionarys, string id)
+        public static object? Increment([NotNull] ParserRuleContext context, Dictionary<string, object?> dictionary, Dictionary<string, object?> dictionarys, string id)
         {
             if (dictionary.ContainsKey(id))
             {
-                if (dictionary[id] == typeof(int))
+                if ((Type?)dictionary[id] == typeof(int))
                 {
                     return dictionarys[id] = Convert.ToInt32(dictionarys[id]) + 1;
                 }
-                else if (dictionary[id] == typeof(float))
+                else if ((Type?)dictionary[id] == typeof(float))
                 {
                     return dictionarys[id] = Convert.ToDouble(dictionarys[id]) + 1;
                 }
                 else
                 {
-                    throw new ArgumentException($"Cannot increment value of type {dictionary[id]?.GetType().Name}");
+                    return ErrorHandler.HandleInvalidIncrementTypeError(context, dictionary, id);
                 }
             }
             else
             {
-                throw new ArgumentException($"Variable {id} does not exist");
+                return ErrorHandler.HandleUndeclaredVariableError(context, dictionary, id);
             }
         }
 
-        public static object? Decrement(Dictionary<string, object?> dictionary, Dictionary<string, object?> dictionarys, string id)
+        public static object? Decrement([NotNull] ParserRuleContext context, Dictionary<string, object?> dictionary, Dictionary<string, object?> dictionarys, string id)
         {
             if (dictionary.ContainsKey(id))
             {
-                if (dictionary[id] == typeof(int))
+                if ((Type?)dictionary[id] == typeof(int))
                 {
                     return dictionarys[id] = Convert.ToInt32(dictionarys[id]) - 1;
                 }
-                else if (dictionary[id] == typeof(float))
+                else if ((Type?)dictionary[id] == typeof(float))
                 {
                     return dictionarys[id] = Convert.ToDouble(dictionarys[id]) - 1;
                 }
                 else
                 {
-                    throw new ArgumentException($"Cannot increment value of type {dictionary[id]?.GetType().Name}");
+                    return ErrorHandler.HandleInvalidDecrementTypeError(context, dictionary, id);
                 }
             }
             else
             {
-                throw new ArgumentException($"Variable {id} does not exist");
+                return ErrorHandler.HandleUndeclaredVariableError(context, dictionary, id); 
             }
         }
     }
